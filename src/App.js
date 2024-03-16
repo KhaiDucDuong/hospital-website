d6a1a420e8a5d3928991b861cc3d27f725fd5bbc
KhaiDucDuong <greenjohnny135@gmail.com>
2024-03-16 18:15:02 +0700

    merge log in, register

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Doctor from "./pages/doctor";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Doctor from "./pages/doctor";
 import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 // import * as jquery from "./assets/js/jquery.min.js";
 // import * as popper from "./assets/js/popper.min.js";
 // import * as bootstrap from"./assets/js/bootstrap.min.js";
@@ -16,9 +18,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Homepage/>}></Route>
           <Route path='/doctor' element={<Doctor />}></Route>
-          {/* <Route path='/register' element={<Register/>}></Route>
+          <Route path='/register' element={<Register/>}></Route>
           <Route path='/login' element={<Login/>}></Route>
-          <Route path='/admin' element={<AdminHome/>}></Route>
+          {/* <Route path='/admin' element={<AdminHome/>}></Route>
           <Route path='/patient/booking' element={<Booking/>}></Route>
           <Route path='/patient/search' element={<SearchingDoctor/>}></Route>
           <Route path='/patient/doctor-profile' element={<DoctorProfile/>}></Route>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Header from '../components/Homepage/Header';
+import Header from '../components/Header';
 import HomeBanner from '../components/Homepage/HomeBanner';
-import Footer from '../components/Homepage/Footer';
+import Footer from '../components/Footer';
 
 function Homepage() {
   return (
